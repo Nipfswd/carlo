e8bfcae4a745b5d40096b697fb894c539e116e27
Nipfswd <allaj0412@gmail.com>
2026-02-25 19:09:08 +0100

    [core] basic dialog
    * UAC elevation and Aero controls enabled

---
+++ resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by usbdos.rc
+//
+#define IDD_DIALOG                      101
+#define IDC_DEVICE                      1001
+#define IDC_FILESYSTEM                  1002
+#define IDC_START                       1003
+#define IDC_CLOSE                       1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
